a97cd8ff238dc5701db1355dce0938c73c40431a
nick031 <nick031@gmail.com>
2013-04-10 19:05:37 +0200

    Fix of the conflicts with packages.

--- src/assignment2/AppClass.java
+++ src/assignment2/AppClass.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.InfModel;
@@ -26,12 +25,12 @@ public class AppClass {
 	private final String owlPath;
 	private OntModel ontology;
 	private InfModel inf;
-	public final static Map<String,String> MIBMAP;
-	public final static String NS = "http://www.item.ntnu.no/fag/ttm4128/sematicweb-2013#";
+	public final static Map<String,String> MIBobjectsMap;
+	public final static String NS = "http://www.item.ntnu.no/fag/ttm4128/sematicweb-2013#";	
 
 	static {
 		Map<String, String> mibMap = new HashMap<String,String>();
-		mibMap .put("hrSystemDate", "HOST-RESOURCES-MIB");
+		mibMap.put("hrSystemDate", "HOST-RESOURCES-MIB");
 		mibMap.put("hrSystemMaxProcesses", "HOST-RESOURCES-MIB");
 		mibMap.put("hrSystemNumUsers", "HOST-RESOURCES-MIB");
 		mibMap.put("hrSystemUptime", "HOST-RESOURCES-MIB");
@@ -41,7 +40,7 @@ public class AppClass {
 		mibMap.put("tcpMaxConn","TCP-MIB");		
 		mibMap.put("udpNoPorts","UDP-MIB");                
 		mibMap.put("udpOutDatagrams","UDP-MIB");
-		MIBMAP = Collections.unmodifiableMap(mibMap);
+		MIBobjectsMap = Collections.unmodifiableMap(mibMap);
 	}
 
 	public AppClass(String owlPath,String agent) {
@@ -52,27 +51,35 @@ public class AppClass {
 		Model schema = FileManager.get().loadModel(this.owlPath);
 		Reasoner reason = ReasonerRegistry.getOWLReasoner();
 		reason = reason.bindSchema(schema);
-		this.inf = ModelFactory.createInfModel(reason,ontology);
-		Resource nForce = this.inf.getResource(NS+"hrSystemUptime");
-		printStatements(this.inf, nForce, null, null);
+		this.inf = ModelFactory.createInfModel(reason, ontology);		
 	}
 
-	public void printStatements(Model m,Resource s, Property p, Resource o) {
-		for (StmtIterator i = m.listStatements(s,p,o);i.hasNext();) {
-			Statement stmt = i.nextStatement();
-			System.out.println(" - " + PrintUtil.print(stmt));
+	public String mibObjectFinder(Model model, Resource resource, Property p, String s) {
+		for (StmtIterator iterator = model.listStatements(resource,p,s); iterator.hasNext();) {						
+			Statement stmt = iterator.nextStatement();
+			for (Map.Entry<String, String> entry : MIBobjectsMap.entrySet()) {
+				if (PrintUtil.print(stmt).contains(entry.getKey())) {					
+					return entry.getKey();
+				}
+			}		
 		}
-	}        
+		return "MIB-Object not found.";			
+	}
+	        
+	public String getMIBobjectValue(String in) throws NullPointerException {
+		Resource nForce = this.inf.getResource(NS+in);
+		String mib = mibObjectFinder(this.inf, nForce, null, null);
 
-	public String getValue(String in) throws NullPointerException {
-		String mib = getMibObjectName(in);
-
+		System.out.println("MIB Module: "+MIBobjectsMap.get(mib));
+		System.out.println("MIB Object: "+mib);
+		
 		ProcessBuilder pb = new ProcessBuilder(
 				"snmpgetnext",
 				"-v2c", "-cttm4128", "-Ov",
 				agent,
-				MIBMAP.get(mib)+"::"+mib
-			);
+				MIBobjectsMap.get(mib)+"::"+mib
+		);
+		
 		try {
 		String a = null;
 		Process p = pb.start();
@@ -83,22 +90,12 @@ public class AppClass {
 		return a.substring(a.indexOf(":")+1).trim();
 		} catch (IOException e) { return null; }
 	}
-
-	public String getMibObjectName(String in) throws NullPointerException {
-		OntClass cls = ontology.getOntClass(NS+in);
-		Resource aliases = inf.getResource(NS+"aliases");
-		if(cls.hasSuperClass(aliases)) {
-			return cls.getSuperClass().getLocalName();
-		} else {
-			return cls.getLocalName();
-		}
-	}
-
+	
 	public static void main(String[] args) {
-		//BasicConfigurator.configure();
 		String agent = "129.241.209.30";
 		String owlPath = "data/sematicweb-2013-new.owl";
 		AppClass a = new AppClass(owlPath ,agent);
-		System.out.println(a.getValue("defttl"));
-	}        
+		String cnmpObject = "x5982";
+		System.out.println(a.getMIBobjectValue(cnmpObject));
+	}
 }
