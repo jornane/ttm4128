dc25de71aa65381b21f491855b54db449cbfd479
yorn <git@yorn.priv.no>
2013-04-17 00:48:24 +0200

    Revamped AppClass
    - CNMP/Module mapping was provided in assignment, using that instead of
    SNMP/Module mapping
    - Removed poor man's statement parsing (string contains), using
    predicate reading
    - Added documentation

--- src/assignment2/AppClass.java
+++ src/assignment2/AppClass.java
@@ -3,52 +3,64 @@ package assignment2;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.InfModel;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.reasoner.Reasoner;
 import com.hp.hpl.jena.reasoner.ReasonerRegistry;
 import com.hp.hpl.jena.util.FileManager;
-import com.hp.hpl.jena.util.PrintUtil;
 
 public class AppClass {
+	/** Address of the agent (IP address or hostname, is used for the snmpgetnext command) */
 	protected final String agent;
+	/** The path to the OWL file containing the inferences */
 	protected final String owlPath;
-	protected final OntModel ontology;
+	/** The inference model */
 	protected final InfModel inf;
+	/** Verbosity of the CLI application */
 	protected boolean verbose;
 	
+	/** Mapping of all CNMP object names to the corresponding MIB module name. */
 	public static final Map<String,String> MIBMODULES;
+
+	/** Default namespace */
 	public static final String NS = "http://www.item.ntnu.no/fag/ttm4128/sematicweb-2013#";
+	/** Default path to the OWL file */
 	public static final String OWLPATH = "data/sematicweb-2013-new.owl";
+	/** Default agent address */
 	public static final String AGENT = "129.241.209.30";
 	
+	/**
+	 * Fill the MIBMODULES map
+	 */
 	static {
 		Map<String, String> mibMap = new HashMap<String,String>();
-		mibMap.put("hrSystemDate", "HOST-RESOURCES-MIB");
-		mibMap.put("hrSystemMaxProcesses", "HOST-RESOURCES-MIB");
-		mibMap.put("hrSystemNumUsers", "HOST-RESOURCES-MIB");
-		mibMap.put("hrSystemUptime", "HOST-RESOURCES-MIB");
-		mibMap.put("snmpEnableAuthenTraps","SNMPv2-MIB");
-		mibMap.put("snmpInPkts", "SNMPv2-MIB");
-		mibMap.put("tcpInSegs","TCP-MIB");
-		mibMap.put("tcpMaxConn","TCP-MIB");
-		mibMap.put("udpNoPorts","UDP-MIB");
-		mibMap.put("udpOutDatagrams","UDP-MIB");
+		mibMap.put("x1234", "HOST-RESOURCES-MIB");
+		mibMap.put("x6742", "HOST-RESOURCES-MIB");
+		mibMap.put("x4912", "SNMPv2-MIB");
+		mibMap.put("x5982", "SNMPv2-MIB");
+		mibMap.put("x5178", "TCP-MIB");
+		mibMap.put("x3125", "TCP-MIB");
 		MIBMODULES = Collections.unmodifiableMap(mibMap);
 	}
 
+	/**
+	 * Instantiate a new CNMP/SNMP converter
+	 * @param owlPath @see {@link #owlPath}
+	 * @param agent	@see {@link #agent}
+	 */
 	public AppClass(String owlPath,String agent) {
 		this.agent = agent;
 		this.owlPath = owlPath;
@@ -56,7 +68,7 @@ public class AppClass {
 		// Redirect errors away
 		PrintStream err = System.err;
 		System.setErr(new PrintStream(new ByteArrayOutputStream()));
-		ontology = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_MICRO_RULE_INF,null);
+		OntModel ontology = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_MICRO_RULE_INF,null);
 		// Reset error redirection
 		System.setErr(err);
 		
@@ -67,24 +79,54 @@ public class AppClass {
 		this.inf = ModelFactory.createInfModel(reason, ontology);		
 	}
 
-	public String mibObjectFinder(Model model, Resource resource, Property p, String s) {
-		for (StmtIterator iterator = model.listStatements(resource,p,s); iterator.hasNext();) {
+	/**
+	 * Retrieves an object name that is not equal to the subject or any generic parent.
+	 * @param model	The model containing the inferences.
+	 * @param subject	An object name that should have another name inferred.
+	 * @param parentClasses	Known names for parent classes (like Thing, SNMP_Object).
+	 * @return	The inferred name of the subject, or null if no inference found.
+	 * 			Never returns the subject or any of the values in parentClasses.
+	 */
+	public String resolveInference(Model model, String subject, String... parentClasses) {
+		StringBuilder parentPattern = new StringBuilder();
+		for(String parentClass : parentClasses)
+			parentPattern.append("|"+Pattern.quote(parentClass));
+		for (StmtIterator iterator = model.listStatements(
+					model.getResource(NS+subject),
+					null,
+					(String)null);
+				iterator.hasNext();)
+		{
 			Statement stmt = iterator.nextStatement();
-			for (Map.Entry<String, String> entry : MIBMODULES.entrySet()) {
-				if (PrintUtil.print(stmt).contains(entry.getKey())) {
-					return entry.getKey();
-				}
-			}		
+			try {
+				String stmtPredicate = stmt.getPredicate().getLocalName();
+				if (!"subClassOf".equals(stmtPredicate))
+					continue;
+				String stmtSubject = new URI(stmt.getSubject().toString()).getFragment();
+				String stmtObject = new URI(stmt.getObject().toString()).getFragment();
+				if (stmtObject.matches("("+Pattern.quote(stmtSubject)+parentPattern+")"))
+					continue;
+				return stmtObject;
+			} catch (URISyntaxException e) {/* do nothing */}
 		}
-		return "MIB-Object not found.";			
+		return null;			
 	}
 	
-	public MibObject getMIBObjectValue(String in) throws NullPointerException {
-		Resource nForce = this.inf.getResource(NS+in);
-		String mib = mibObjectFinder(this.inf, nForce, null, null);
+	/**
+	 * Return the SNMP output for a CNMP object
+	 * @param in	Name of the CNMP object
+	 * @return	output of snmpgetnext for the corresponding SNMP object
+	 * 		Returns NULL if the SNMP name cannot be determined,
+	 * 		or if something goes wrong executing snmpgetnext.
+	 */
+	public MibObject getMIBObjectValue(String in) {
+		String mib = resolveInference(this.inf, in, "SNMP_Object", "CNMP_Object", "Thing", "Resource");
+		String module = MIBMODULES.get(in);
+		if (module == null)
+			return null;
 
 		if (verbose) {
-			System.out.println("MIB Module: "+MIBMODULES.get(mib));
+			System.out.println("MIB Module: "+module);
 			System.out.println("MIB Object: "+mib);
 		}
 		
@@ -92,7 +134,7 @@ public class AppClass {
 				"snmpgetnext",
 				"-v2c", "-cttm4128",
 				agent,
-				MIBMODULES.get(mib)+"::"+mib
+				module+"::"+mib
 		);
 		
 		try {
@@ -108,6 +150,10 @@ public class AppClass {
 		} catch (IOException e) { return null; }
 	}
 	
+	/**
+	 * Execute the program
+	 * @param args	Commandline arguments
+	 */
 	public static void main(String[] args) {
 		MibObject mib = getMibObject(args);
 		if (mib == null)
@@ -115,7 +161,12 @@ public class AppClass {
 		else
 			System.out.println(mib);
 	}
-
+	
+	/**
+	 * Program to get an SNMP MibObject from a CNMP request
+	 * @param args	Commandline arguments
+	 * @return	the MibObject
+	 */
 	public static MibObject getMibObject(String[] args) {
 		String agent = AGENT;
 		String owlPath = OWLPATH;
@@ -143,6 +194,8 @@ public class AppClass {
 			cnmpObject = reader.nextLine();
 			reader.close();
 		}
+		if (!MIBMODULES.containsKey(cnmpObject))
+			System.err.println("No module mapping for "+cnmpObject);
 		if (verbose)
 			System.out.println("Processing...");
 		AppClass converter = new AppClass(owlPath, agent);
